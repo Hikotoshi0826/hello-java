908cfa5f4f977f2cca1632cc041d057ca01ff06e
Hikotoshi0826 <hikotoshi826@gmail.com>
2026-07-02 15:52:27 +0900

    Add Hello2

--- hello.java
+++ hello.java
@@ -1,1 +1,1 @@
-'public class Hello { public static void main(String[] args) { System.out.println("Hello, GitHub!"); } }' 
+'public class Hello { public static void main(String[] args) { System.out.println("Hello, GitHub!");System.out.println("Hello2, GitHub!!"); } }' 
